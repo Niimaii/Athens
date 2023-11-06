8a916e7c771755673973d925537223cd25d8714f
Niimaii <nimaicgomes@gmail.com>
2023-11-06 07:05:23 -0300

    users can stack timeout

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,23 @@ client.on('messageCreate', (message) => {
 });
 
 client.on('messageReactionAdd', (reaction, user) => {
-  let message = reaction.message;
+  const timeout = reaction.message.member.communicationDisabledUntil;
+  const now = new Date();
+  const lastTimedOut = new Date(timeout);
+
+  //   If when last timed out was prior to now then make me 0, else make me equal to the difference in milliseconds
+  let timeoutLeft =
+    now.getTime() > lastTimedOut.getTime()
+      ? 0
+      : lastTimedOut.getTime() - now.getTime();
   let emoji = reaction.emoji;
+
+  console.log(timeoutLeft);
 
   if (emoji.id == '1170550414689714197') {
     // Timeout user who's message was reacted to
     console.log(`${user.username} reacted`);
-    reaction.message.member.timeout(2.5 * 60 * 1000);
+    // Add 2:30mins + the current timeout
+    reaction.message.member.timeout(timeoutLeft + 2.5 * 60 * 1000);
   }
 });
